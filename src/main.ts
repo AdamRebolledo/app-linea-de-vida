515803f625d26d80cadee701a81cb8a28671e298
Efrain Garay <egaray7@gmail.com>
2019-08-10 13:42:27 -0400

    feat(pages): entrega v 0.0.1

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+interface Scripts {
+  name: string;
+  src: string;
+}
+
+export const ScriptStore: Scripts[] = [
+  { name: 'pagejs', src: '../assets/js/page.min.js' }
+];
+
 if (environment.production) {
   enableProdMode();
 }
